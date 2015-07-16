07404428dbc0aefe92ba183bf04598de4fc9d827
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-15 19:06:26 -0500

    Created login view

--- controllers/user_controller.rb
+++ controllers/user_controller.rb
@@ -1,3 +1,18 @@
+set :sessions => true
+
+before do
+  if session["user_id"] == nil
+    erb :"/user/login"
+  else
+    @user = User.find(session["user_id"])
+  end
+end
+
+get "/logout" do
+  session["user_id"] = nil
+  erb :"/home"
+end
+
 get "/add_new_user" do
   erb :"/user/add_user_form"
 end
@@ -9,4 +24,8 @@ end
 
 get "/see_all_users" do
   erb :"/user/see_all_users"
+end
+
+get "/login" do
+  erb :"/user/login"
 end
